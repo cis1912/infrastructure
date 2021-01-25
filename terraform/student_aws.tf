4a24fe6e5c4e5b6f723525197989339d5d9f728a
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-01-25 16:00:22 -0500

    typo

--- terraform/student_aws.tf
+++ terraform/student_aws.tf
@@ -1,6 +1,6 @@
 // Configure all student aws accounts
 module "aws_accounts" {
-  for_each = local.students
+  for_each = var.students
   source   = "./modules/aws_account"
   pennkey  = each.key
 }
