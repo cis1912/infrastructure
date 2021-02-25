4302d6b85d4c4ed56e42e0ba8c67d31545627fa4
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-02-24 23:23:37 -0500

    Provider config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,21 +2,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
-// provider "helm" {
-//   kubernetes {
-//     load_config_file       = false
-//     host                   = data.aws_eks_cluster.eks.endpoint
-//     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
-//     token                  = data.aws_eks_cluster_auth.eks.token
-//   }
-// }
+provider "helm" {
+  kubernetes {
+    load_config_file       = false
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
 
-// provider "kubernetes" {
-//   load_config_file       = false
-//   host                   = data.aws_eks_cluster.eks.endpoint
-//   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
-//   token                  = data.aws_eks_cluster_auth.eks.token
-// }
+provider "kubernetes" {
+  load_config_file       = false
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 provider "github" {
   organization = "cis188"
 }
