b4d5dd8c2462ebb278eab3068e455a836a64a2bf
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-02-28 18:54:01 -0500

    Add var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "students" {
   type        = set(string)
   description = "Student pennkeys"
 }
+
+variable "image_pull_pat" {
+  type        = string
+  description = "PAT of GH User to pull gcr images"
+}
