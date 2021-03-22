9af64e2ec0ca32a2e360d82bb39bafc4ecf11d9d
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-03-22 16:40:27 -0400

    Configure pod limits

--- terraform/modules/k8s_config/main.tf
+++ terraform/modules/k8s_config/main.tf
@@ -21,3 +21,17 @@ resource "kubernetes_role_binding" "rb" {
     api_group = "rbac.authorization.k8s.io"
   }
 }
+
+// Restrict number of pods
+resource "kubernetes_resource_quota" "pod-limit" {
+  metadata {
+    name = "pod-limit"
+    namespace = kubernetes_namespace.ns.metadata[0].name
+  }
+  spec {
+    hard = {
+      pods = 10
+    }
+    scopes = ["BestEffort"]
+  }
+}
