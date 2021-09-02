5343e45d99358466eab878f7d570fd27c8881727
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-09-02 13:49:02 -0400

    Prep for next semester

--- terraform/github_bot.tf
+++ terraform/github_bot.tf
@@ -2,6 +2,10 @@
 resource "github_membership" "bot" {
   username = local.bot_user
   role     = "admin"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 // Create master GitHub homework repositories
@@ -11,6 +15,10 @@ resource "github_repository" "hw" {
   description = "${each.key} stubbed template"
   visibility  = "private"
   is_template = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 // Grant bot user push access to hw repos
@@ -18,12 +26,20 @@ resource "github_team" "bot" {
   name        = "bot"
   description = "Bot team"
   privacy     = "closed"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "github_team_membership" "bot" {
   team_id  = github_team.bot.id
   username = local.bot_user
   role     = "member"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "github_team_repository" "bot" {
@@ -31,4 +47,8 @@ resource "github_team_repository" "bot" {
   team_id    = github_team.bot.id
   repository = each.key
   permission = "push"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
