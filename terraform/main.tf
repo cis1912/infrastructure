2c1dc3263a04813e727da4e234ac0afdafddd81d
Joy Liu <joyliu.q@gmail.com>
2022-09-08 18:01:46 -0400

    :rotating_light: Push breaking code to test why terraform has errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 locals {
   // Name of the homework mapped to published status
   published = {
-    hw0           = true
-    hw1           = true
-    hw2           = true
-    hw3           = true
-    hw4           = true
+    hw0           = false
+    hw1           = false
+    hw2           = false
+    hw3           = false
+    hw4           = false
     final_project = false
   }
   // Create a list of objects representing all homework repos to be (eventually) created. 
@@ -18,5 +18,12 @@ locals {
   ])
   bot_user         = "cis188bot"
   k8s_cluster_name = "cis188"
-  users            = merge(var.students, var.instructors, var.tas)
+  tas = {
+    "joyliu" = "joyliu-q"
+  }
+  instructors = {
+    "cphalen" = "cphalen"
+    "grohan"  = "rohangpta"
+  }
+  users = merge(var.students, var.instructors, var.tas)
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,6 @@ provider "helm" {
 }
 
 provider "kubernetes" {
-  load_config_file       = false
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.eks.token
@@ -25,7 +24,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.18"
+      version = "~> 4.0"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -33,10 +32,10 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 1.13"
+      version = "~> 2.0"
     }
     github = {
-      source  = "hashicorp/github"
+      source  = "integrations/github"
       version = "~> 4.1"
     }
   }
